a9fd0c208d6da2b99f9f0f40dfd515dc723d98dc
daniellen23 <danielle.nguyen23@ycdsbk12.ca>
2021-02-15 07:59:00 +0000

    initial coding

--- problem2.py
+++ problem2.py
@@ -1,1 +1,18 @@
+"""
+Name: problem2.py
 
+Purpose: Tell whether you can take a summer vacation to Europe or California based on whether you get an 80% average and make $500 or not.
+
+Author: Nguyen.D
+
+Date: 2021-02-14
+"""
+average_percentage = int(input("What is your average grade percentage? "))
+earned_money = int(input("How much did you make before the summer? "))
+
+if average_percentage >= 80 and earned_money >= 500:
+    print("You can go to Europe")
+elif average_percentage >= 80 and earned_money < 500 :
+    print("You can go to California.")
+else:
+  print("You can't go anywhere.")
